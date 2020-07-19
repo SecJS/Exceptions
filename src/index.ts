9105d0d86eccf54c831a8c24a3267a4595a465da
João Lenon <lenonsec7@gmail.com>
2020-07-19 12:47:15 -0300

    feat: good first issue

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { NotFoundException } from './Exceptions/NotFoundException'
+export { UnauthorizedException } from './Exceptions/UnauthorizedException'
+export { InternalErrorException } from './Exceptions/InternalErrorException'
+export { InvalidArgsException } from './Exceptions/InvalidArgsException'
